8843fc8f7f07c1a48f05d22b85e3af5186679195
EmanuelErnesto <emanuel.earego@gmail.com>
2024-03-14 15:36:27 -0300

    comentarios retirados

--- BACKEND/routes/users.js
+++ BACKEND/routes/users.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 
 const userController = require("../controllers/userController");
-const userRouter = require("../controllers/userController");
 
 router
 .route("/users")
