d90cffbd48a05ef49bbd3ef57b3707c4873701e0
marlonportotorres4 <marlonptorres2004@gmail.com>
2024-03-28 17:34:30 -0300

    Completado o design da home, adicionado a função de rolagem e adicionado o header nas outras telas

--- FRONTEND/src/pages/Projects/index.js
+++ FRONTEND/src/pages/Projects/index.js
@@ -1,20 +1,47 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
+import { StyleSheet, View, Image } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
 
 export function Projects() {
   return (
+    <View style={styles.container}>
+    <View>
+    <LinearGradient
+      colors={["#5de0e6", "#004aad"]}
+      start={{ x: 0.5, y: 0.0 }}
+      end={{ x: 0.5, y: 1.0 }}
+      style={styles.gradiente}
+    >
+      <Image
+        style={styles.logo}
+        source={require("../../../assets/logo-recifeajuda+.png")}
+      />
+    
+    </LinearGradient>
+  </View>
 
-      <View style={styles.container}>
-        
-      </View>
-  );
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+container: {
+flex: 1,
+},
+gradiente: {
+flex: 0,
+height: 100,
+borderBottomRightRadius: 20,
+borderBottomLeftRadius: 20,
+alignItems: "center",
+},
+logo: {
+width: 130,
+height: 130,
+top: -5,
+right: 130,
+margin: 0,
+},
 });
 
 export default Projects;
--- FRONTEND/src/pages/info/index.js
+++ FRONTEND/src/pages/info/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
+import { StyleSheet, View, Image } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
 
 export function Info() {
   return (
 
       <View style={styles.container}>
+        <View>
+        <LinearGradient
+          colors={["#5de0e6", "#004aad"]}
+          start={{ x: 0.5, y: 0.0 }}
+          end={{ x: 0.5, y: 1.0 }}
+          style={styles.gradiente}
+        >
+          <Image
+            style={styles.logo}
+            source={require("../../../assets/logo-recifeajuda+.png")}
+          />
         
+        </LinearGradient>
+      </View>
+
       </View>
   );
 }
@@ -14,6 +28,20 @@ export function Info() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  gradiente: {
+    flex: 0,
+    height: 100,
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    alignItems: "center",
+  },
+  logo: {
+    width: 130,
+    height: 130,
+    top: -5,
+    right: 130,
+    margin: 0,
   },
 });
 
